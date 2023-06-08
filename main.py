6ba7a043e032599ab7a52a2d4f49edf3b4278a79
translate-to-communicate <chris.a.romeo@gmail.com>
2023-06-08 09:57:08 -0400

    June 8 Update 02

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# Updated 07JUN2023 10:04
+# Updated 08JUN2023 09:56
 # Author: Christopher Romeo
 # This is the working branch for naming the input files using a prompt for the user
+# The script now prompts the user to identify the responsible agency for each file
 import sys
 from IPython.display import display
 import tkinter as tk
 from tkinter import *
+from tkinter.simpledialog import askstring
+from tkinter.messagebox import showinfo
 from tkinter import ttk
 import tkinter.messagebox as mbox
 from tkinter import filedialog
@@ -33,23 +36,23 @@ def main():
     # ipath is the input directory path and opath is the output directory path
     # There will be two versions of this directory information to allow for faster testing
 
-    ipath = Tk()
-    ipath.withdraw()
-    ipath.directory = filedialog.askdirectory(initialdir="C:/", title="Input Directory for CFS Files")
-    print('The chosen input directory is: ' + ipath.directory)
+    # ipath = Tk()
+    # ipath.withdraw()
+    # ipath.directory = filedialog.askdirectory(initialdir="C:/", title="Input Directory for CFS Files")
+    # print('The chosen input directory is: ' + ipath.directory)
+    #
+    # opath = Tk()
+    # opath.withdraw()
+    # opath.directory = filedialog.askdirectory(initialdir="C:/", title="Output Directory for CFS Translation Results")
+    # print('The chosen output directory is: ' + opath.directory)
 
-    opath = Tk()
-    opath.withdraw()
-    opath.directory = filedialog.askdirectory(initialdir="C:/", title="Output Directory for CFS Translation Results")
-    print('The chosen output directory is: ' + opath.directory)
-
-    # ipath = "C:/Users/chris/Desktop/School Assignments/Summer/TEST DATA"
-    # opath = "C:/Users/chris/Desktop/School Assignments/Summer/TEST OUTCOME"
+    ipath = "C:/Users/chris/Desktop/School Assignments/Summer/TEST DATA"
+    opath = "C:/Users/chris/Desktop/School Assignments/Summer/TEST OUTCOME"
 
     # This pulls all files in the chosen directory
     print("Using glob.glob")
-    csv_files_csv = glob.glob(ipath.directory + '/*')  # This is the production code
-    # csv_files_csv = glob.glob(ipath + '/*')  # This is the quick testing code
+    # csv_files_csv = glob.glob(ipath.directory + '/*')  # This is the production code
+    csv_files_csv = glob.glob(ipath + '/*')  # This is the quick testing code
 
     # Show all the files that were identified
     # for file in csv_files_csv:
@@ -69,6 +72,44 @@ def main():
         # print(f)
         agency = os.path.basename(f)
         print(f"Now processing: {agency}")
+
+        # # Generate the user prompt to ensure proper agency information
+        # win = tk.Tk()
+        # win.title('Agency Selection')
+        # win.geometry("")
+        #
+        # leb = ttk.Label(win, text=f'What is the responsible agency for the file: {agency}')
+        # leb.grid(row=0, column=0)
+        #
+        # entb = ttk.Entry(win)
+        # entb.grid(row=0, column=1, columnspan=2)
+        #
+        # def show():
+        #     mbox.showinfo('Agency', f'{entb}')
+        #
+        # btn = ttk.Button(win, text='Show', command=show)
+        # btn.grid(row=1, column=1)
+        # # Also if you want to destroy your GUI it is better to use 'win.destroy()' instead of 'exit()'
+        # btn1 = ttk.Button(win, text='Exit', command=win.destroy)
+        # btn1.grid(row=1, column=2)
+        #
+        # win.mainloop()
+        #
+        #
+        win = Tk()
+        win.withdraw()  # Hides the tk window
+        win.geometry("")  # Allows the window to auto expand based on the following data
+        enta = askstring('Agency', f'What is the responsible agency for the file: {agency}?')
+        win.destroy()  # Ensures the window is closed for performance purposes
+        # This loop ensures that if the user does not input any information, hits cancel, or the 'x' that the   #
+        # agency information does not change and cause an error. This will default to the file basename.        #
+        if enta is '':
+            print(f'No input given for: {agency}')
+        elif enta is None:
+            print(f"No input given for: {agency}")
+        else:
+            agency = enta
+        # showinfo('Agency', f'The responsible agency is: {enta}')
         # Read in the document
         if ".csv" in f:
             # print("This is a csv file")
@@ -80,8 +121,8 @@ def main():
             # Remove any underscores from the column headers
             temp_df = temp_df.rename(columns=lambda name: name.replace('_', ' '))
             # Create a new processed sheet for each agency
-            temp_df.to_csv(f"{opath.directory}/Processed_{agency}", index=False)  # This is the production code
-            # temp_df.to_csv(f"{opath}/Processed_{agency}", index=False)  # This is the one for quick testing only
+            # temp_df.to_csv(f"{opath.directory}/Processed_{agency}", index=False)  # This is the production code
+            temp_df.to_csv(f"{opath}/Processed_{agency}.csv", index=False)  # This is the one for quick testing only
             # add it to the list
             li.append(temp_df)
             #                                                                                                       #
@@ -108,41 +149,40 @@ def main():
             # Now we move on to the actual combination of files into one document
             temp_df = temp_df[temp_df.columns.intersection(final_columns)]
             liz.append(temp_df)
-            # temp_df.to_csv(f"C:/Users/chris/Desktop/School Assignments/Summer/TEST OUTCOME/zz_{agency}", index=False)
-            temp_df.to_csv(f"{opath.directory}/zz_{agency}", index=False)  # This is the production code
-            # temp_df.to_csv(f"{opath}/zz_{agency}", index=False)  # This is the one for quick testing only
+            # temp_df.to_csv(f"{opath.directory}/zz_{agency}", index=False)  # This is the production code
+            temp_df.to_csv(f"{opath}/zz_{agency}.csv", index=False)  # This is the one for quick testing only
             # print(temp_df.dtypes)
         elif ".xlsx" in f:
             # Run the same process as above but for Excel files
             temp_df = pd.read_excel(f)
             temp_df.insert(0, 'Agency', agency)
             temp_df['Agency'] = temp_df['Agency'].replace('.xlsx', '', regex=True)
-            temp_df.to_csv(f"{opath.directory}/Processed_{agency}.csv", index=False)  # This is the production code
-            # temp_df.to_csv(f"{opath}/Processed_{agency}.csv", index=False)  # This is the one for quick testing only
+            # temp_df.to_csv(f"{opath.directory}/Processed_{agency}.csv", index=False)  # This is the production code
+            temp_df.to_csv(f"{opath}/Processed_{agency}.csv", index=False)  # This is the one for quick testing only
             li.append(temp_df)
             # print(f'Successfully created dataframe for {agency} with shape {temp_df.shape}')
             # Now we move on to the actual combination of files into one document
             temp_df.columns = map(str.lower, temp_df.columns)
             temp_df = temp_df[temp_df.columns.intersection(final_columns)]
             liz.append(temp_df)
-            temp_df.to_csv(f"{opath.directory}/zz_{agency}.csv", index=False)  # This is the production code
-            # temp_df.to_csv(f"{opath}/zz_{agency}.csv", index=False)  # This is the one for quick testing only
+            # temp_df.to_csv(f"{opath.directory}/zz_{agency}.csv", index=False)  # This is the production code
+            temp_df.to_csv(f"{opath}/zz_{agency}.csv", index=False)  # This is the one for quick testing only
             # print(temp_df.dtypes)
         elif ".xml" in f:
             # Run the same process as above but for XML files
             temp_df = pd.read_xml(f)
             temp_df.insert(0, 'Agency', agency)
             temp_df['Agency'] = temp_df['Agency'].replace('.xml', '', regex=True)
-            temp_df.to_csv(f"{opath.directory}/Processed_{agency}.csv", index=False)  # This is the production code
-            # temp_df.to_csv(f"{opath}/Processed_{agency}.csv", index=False)  # This is the one for quick testing only
+            # temp_df.to_csv(f"{opath.directory}/Processed_{agency}.csv", index=False)  # This is the production code
+            temp_df.to_csv(f"{opath}/Processed_{agency}.csv", index=False)  # This is the one for quick testing only
             li.append(temp_df)
             print(f'Successfully created dataframe for {agency} with shape {temp_df.shape}')
             # Now we move on to the actual combination of files into one document
             temp_df.columns = map(str.lower, temp_df.columns)
             temp_df = temp_df[temp_df.columns.intersection(final_columns)]
             liz.append(temp_df)
-            temp_df.to_csv(f"{opath.directory}/zz_{agency}.csv", index=False)  # This is the production code
-            # temp_df.to_csv(f"{opath}/zz_{agency}.csv", index=False)  # This is the one for quick testing only
+            # temp_df.to_csv(f"{opath.directory}/zz_{agency}.csv", index=False)  # This is the production code
+            temp_df.to_csv(f"{opath}/zz_{agency}.csv", index=False)  # This is the one for quick testing only
             # print(temp_df.dtypes)
         else:
             # Display a message to indicate the file extension found is not able to be converted at this time
@@ -172,10 +212,10 @@ def main():
     # print(df2.index)
     # df.head()
 
-    df.to_csv(f"{opath.directory}/SingleFile.csv")  # This is the production code
-    df2.to_csv(f"{opath.directory}/zzSingleFile.csv")  # This is the production code
-    # df.to_csv(f"{opath}/SingleFile.csv")  # Testing purposes only
-    # df2.to_csv(f"{opath}/zzSingleFile.csv")  # Testing purposes only
+    # df.to_csv(f"{opath.directory}/SingleFile.csv")  # This is the production code
+    # df2.to_csv(f"{opath.directory}/zzSingleFile.csv")  # This is the production code
+    df.to_csv(f"{opath}/SingleFile.csv")  # Testing purposes only
+    df2.to_csv(f"{opath}/zzSingleFile.csv")  # Testing purposes only
 
     # print(li)
     # alldfs = [var for var in dir() if isinstance(eval(var), pd.core.frame.DataFrame)]
@@ -189,7 +229,7 @@ def main():
     print('')
     print('The following is the first 5 rows from the combined data:')
     print(tabulate(df2.head(5), headers='keys', tablefmt='psql'))
-    input("The process is complete. Please press Enter to quit.")
+    print("The process is complete.")
     quit()
 
     # This is the practice space for the message box that will display the column and first row of the      #
@@ -249,5 +289,6 @@ def main():
     # ctypes.windll.user32.MessageBoxW(0, "The system is ready to start.", "Initiate", 1)
 
     # result = MessageBoxW()
+
 
 main()
